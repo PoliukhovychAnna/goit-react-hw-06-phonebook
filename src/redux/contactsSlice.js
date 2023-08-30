0f6a5ee474e8ab05638a82ece25b9e60cd9e9f16
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-08-30 22:18:55 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -13,7 +13,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-        state = state.unshift(action.payload);
+        state.contacts = state.contacts.unshift(action.payload);
       },
       prepare(data) {
         return {
